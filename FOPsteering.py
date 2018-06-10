44ca873bc1d6f34a3a4bc462374ae74ee2850316
Kedra, Jerzy <jurek.kedra@gmail.com>
2018-06-10 17:31:06 +0200

    Kosmetyczne zmiany & usunięcie starych buttonów

--- FOPsteering.py
+++ FOPsteering.py
@@ -19,54 +19,32 @@ def ifin(trasa, lista):
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        # self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.n = tk.Button(self, text="N", command=self.up)
-        # self.n.pack(side="top")
-
-        self.s = tk.Button(self, text="S", command=self.down)
-        # self.s.pack(side="bottom")
-
-        self.w = tk.Button(self, text="W", command=self.left)
-        # self.w.pack(side="left")
-
-        self.e = tk.Button(self, text="E", command=self.right)
-        # self.e.pack(side="right")
-
-        self.q = tk.Button(self, text="Q", fg="red",
-                           command=quit)
-        # self.quit.pack(side="bottom")
         self.button_grid = tk_tools.ButtonGrid(root, 3,
                                                ['Column0',
                                                 'Column1',
                                                 'Column2'])
-
         bg = self.button_grid
 
         bg.add_row([
-            ("nW", lambda: self.leftup()),
-            ("N", lambda: self.up()),
-            ("nE", lambda: self.rightup())
-        ])
+                    ("nW", lambda: self.leftup()),
+                    ("N", lambda: self.up()),
+                    ("nE", lambda: self.rightup())
+                  ])
 
-        bg.add_row \
-                (
-                [
+        bg.add_row([
                     ("W", lambda: self.left()),
                     ("Menu", lambda: self.quit()),
                     ("E", lambda: self.right())
-                ]
-            )
-        self.button_grid.add_row \
-                (
-                [
+                  ])
+
+        bg.add_row([
                     ("W", lambda: self.left()),
                     ("Menu", lambda: self.quit()),
                     ("E", lambda: self.right())
-                ]
-            )
+                  ])
 
     def quit(self):
         sys.exit()
